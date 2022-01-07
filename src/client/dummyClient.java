94ece4f97ee4c78bf03a5181972e76f5f4b86e14
alpblkba <alp.bolukbasi@ozu.edu.tr>
2022-01-07 18:48:43 +0300

    md5 finished, RTT added, download eventuated

--- src/client/dummyClient.java
+++ src/client/dummyClient.java
@@ -1,17 +1,14 @@
 package client;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
 import java.nio.ByteBuffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.DigestInputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 import model.FileDataResponseType;
 import model.FileListResponseType;
@@ -19,26 +16,25 @@ import model.FileSizeResponseType;
 import model.RequestType;
 import model.ResponseType;
 import model.ResponseType.RESPONSE_TYPES;
-import client.loggerManager;
 
 public class dummyClient {
 
-	private void sendInvalidRequest(String ip, int port) throws IOException{
-		 InetAddress IPAddress = InetAddress.getByName(ip); 
-         RequestType req=new RequestType(4, 0, 0, 0, null);
-         byte[] sendData = req.toByteArray();
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, port);
-         DatagramSocket dsocket = new DatagramSocket();
-         dsocket.send(sendPacket);
-         byte[] receiveData=new byte[ResponseType.MAX_RESPONSE_SIZE];
-         DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
-         dsocket.receive(receivePacket);
-         ResponseType response=new ResponseType(receivePacket.getData());
-         loggerManager.getInstance(this.getClass()).debug(response.toString());
-	}
-	
-	private void getFileList(String ip, int port) throws IOException{
-		InetAddress IPAddress = InetAddress.getByName(ip); 
+    void sendInvalidRequest(String ip, int port) throws IOException{
+        InetAddress IPAddress = InetAddress.getByName(ip);
+        RequestType req=new RequestType(4, 0, 0, 0, null);
+        byte[] sendData = req.toByteArray();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, port);
+        DatagramSocket dsocket = new DatagramSocket();
+        dsocket.send(sendPacket);
+        byte[] receiveData=new byte[ResponseType.MAX_RESPONSE_SIZE];
+        DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
+        dsocket.receive(receivePacket);
+        ResponseType response=new ResponseType(receivePacket.getData());
+        loggerManager.getInstance(this.getClass()).debug(response.toString());
+    }
+
+    private void getFileList(String ip, int port) throws IOException{
+        InetAddress IPAddress = InetAddress.getByName(ip);
         RequestType req=new RequestType(RequestType.REQUEST_TYPES.GET_FILE_LIST, 0, 0, 0, null);
         byte[] sendData = req.toByteArray();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, port);
@@ -51,9 +47,9 @@ public class dummyClient {
         loggerManager.getInstance(this.getClass()).debug(response.toString());
     }
 
-	
-	private long getFileSize(String ip, int port, int file_id) throws IOException{
-		InetAddress IPAddress = InetAddress.getByName(ip); 
+
+    long getFileSize(String ip, int port, int file_id) throws IOException{
+        InetAddress IPAddress = InetAddress.getByName(ip);
         RequestType req=new RequestType(RequestType.REQUEST_TYPES.GET_FILE_SIZE, file_id, 0, 0, null);
         byte[] sendData = req.toByteArray();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, port);
@@ -65,10 +61,10 @@ public class dummyClient {
         FileSizeResponseType response=new FileSizeResponseType(receivePacket.getData());
         loggerManager.getInstance(this.getClass()).debug(response.toString());
         return response.getFileSize();
-	}
-	
-	private void getFileData(String ip, int port, int file_id, long start, long end) throws IOException{
-		InetAddress IPAddress = InetAddress.getByName(ip); 
+    }
+
+    private void getFileData(String ip, int port, int file_id, long start, long end) throws IOException{
+        InetAddress IPAddress = InetAddress.getByName(ip);
         RequestType req=new RequestType(RequestType.REQUEST_TYPES.GET_FILE_DATA, file_id, start, end, null);
         byte[] sendData = req.toByteArray();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, port);
@@ -77,19 +73,47 @@ public class dummyClient {
         byte[] receiveData=new byte[ResponseType.MAX_RESPONSE_SIZE];
         long maxReceivedByte=-1;
         while(maxReceivedByte<end){
-        	DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
+            DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
             dsocket.receive(receivePacket);
             FileDataResponseType response=new FileDataResponseType(receivePacket.getData());
             loggerManager.getInstance(this.getClass()).debug(response.toString());
             if (response.getResponseType()!=RESPONSE_TYPES.GET_FILE_DATA_SUCCESS){
-            	break;
+                break;
             }
             if (response.getEnd_byte()>maxReceivedByte){
-            	maxReceivedByte=response.getEnd_byte();
+                maxReceivedByte=response.getEnd_byte();
             };
         }
-	}
 
+    }
+
+    public static long getFileBytes(dummyClient inst, int fileId, int port, String ip) throws IOException {
+
+        return inst.getFileSize(ip, port, fileId);
+    }
+
+    //The client must also compute the md5sum of the received file and output it on the screen.
+    // We will use this md5 hash to make sure the data was fully downloaded and is identical to the source
+
+    public static String md5Sum(byte[] data) throws IOException, NoSuchAlgorithmException {
+        if (data == null)
+            return "null";
+        try {
+            String s = "";
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            data = md.digest();
+            StringBuffer sb = new StringBuffer();
+
+            for (int i = 0; i < data.length; ++i) {
+                sb.append(Integer.toHexString((data[i] & 0xFF) | 0x100).substring(1,3));
+            }
+
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //bu methodu mainde güzel bir yere daha sonra yerleştireceğiz
     public boolean RTT(dummyClient inst, String ip, int port1, int port2) throws IOException{
         long t1_before = System.currentTimeMillis();
         inst.sendInvalidRequest(ip,port1);
@@ -104,38 +128,30 @@ public class dummyClient {
         if (t1 <= t2){ return true;}
         else{ return false;}
     }
-/*
-    public String MD5(String md5) {
-        String md5string = "";
-        try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(md5.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-            }
-            return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
-            return null;
-        }
+
+
+    public static byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
     }
 
-*/
-
     public static void main(String[] args) throws Exception{
+        int theFastestProt;
+        long start_byte = 1;
+        long packetByte = 1000;
+
         Scanner scanner = new Scanner(System.in);
-
-
-		if (args.length<1){
-			throw new IllegalArgumentException("ip:port is mandatory");
-		}
-		String[] adr=args[0].split(":");
-		String ip=adr[0];
-		int port1=Integer.valueOf(adr[1]);
+        if (args.length<1){
+            throw new IllegalArgumentException("ip:port is mandatory");
+        }
+        String[] adr=args[0].split(":");
+        String ip=adr[0];
+        int port1=Integer.valueOf(adr[1]);
         int port2=Integer.valueOf(adr[2]);
 
 
-		dummyClient inst=new dummyClient();
+        dummyClient inst=new dummyClient();
 
 
         inst.sendInvalidRequest(ip,port1);
@@ -144,33 +160,124 @@ public class dummyClient {
         int file_id = scanner.nextInt();
         long file_size = inst.getFileSize(ip, port1, file_id);
         System.out.println("File "+file_id+" has been selected. Getting the size information…");
+
+        if(inst.RTT(inst, ip, port1, port2)){ theFastestProt = port1;}
+        else { theFastestProt = port2;}
+
+        byte[] file_size_AsByte = longToBytes(file_size);  //to calculate md5sum
+
+        myThread thread = new myThread(file_id, ip, theFastestProt, inst, -1, -1);
+        thread.run();
+
         System.out.println("File "+file_id+"is "+file_size+" bytes. Starting to download…");
+
+        long end_byte = packetByte;
+        int port;
+        long timer;
+        var x = 1;
+
+        long time = System.currentTimeMillis();
+
+        while (start_byte < file_size) {
+
+            if (file_size - start_byte > 1000) {
+
+                myThread thread2 = new myThread(file_id, ip, theFastestProt, inst, start_byte, end_byte);
+                thread2.run();
+
+                timer = thread2.getTimer();
+
+                if (timer == 0) timer = 1;
+
+                System.out.println("File is downloading in port no: " + theFastestProt + "\n" + "Downloading in speed (Bytes / ms): " + (packetByte / timer) + "\n" + "Time passed: " + (System.currentTimeMillis() - time) + " ms \n");
+
+                start_byte += packetByte;
+                end_byte += packetByte;
+                x++;
+
+            } else {
+                myThread thread3 = new myThread(file_id, ip, theFastestProt, inst, start_byte, file_size);
+                thread3.run();
+
+                System.out.println("File " + file_id + " has been downloaded in " + (System.currentTimeMillis() - time) + " ms. in port " + theFastestProt);
+
+                System.out.println("Download completed.");
+                break;
+            }
+        }
+
+        String md5sum = md5Sum(file_size_AsByte);
+
+        System.out.println("File " + file_id + " has been downloaded in " + (System.currentTimeMillis() - time) + " ms. The md5 hash is  " + md5sum);
+        System.exit(0);
+
+        /*
         if(inst.RTT(inst, ip, port1, port2)){ inst.getFileData(ip, port1, file_id, 1, file_size);}
         else { inst.getFileData(ip, port2, file_id, 1, file_size);}
+        System.out.println(file_size);*/
 
-        System.out.println(file_size);
-
-
-        /*
-        System.out.println(System.getProperty(String.valueOf(file_id)));
-        System.out.println("file id aldi");
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        InputStream is = Files.newInputStream(Paths.get(String.valueOf(file_id)));
-        DigestInputStream dis = new DigestInputStream(is, md);
-        byte[] digest = md.digest();
-        System.out.println("File" +file_id+ "has been downloaded in 32345 ms. The md5 hash is " + md.digest());
-        //kod bu. bunun çalışması için download edilen kodu bulmamız ve sonra Paths.get() içerisine path'ini yazmamız gerekiyor.
-        //uzaktaki dosyayı hashleyemiyoruz
-        */
-
-        /*
-        inst.sendInvalidRequest(ip1,port1);
-		inst.getFileList(ip1,port1);
-		inst.getFileSize(ip1,port1,0);
-		long size=inst.getFileSize(ip1,port1,1);
-		inst.getFileData(ip1,port1,0,0,1);
-		inst.getFileData(ip1,port1,1,30,20);
-		inst.getFileData(ip1,port1,1,1,size);
-        */
-	}
+    }
 }
+
+class myThread extends Thread {
+
+    int fileId;
+    String ip;
+    int port;
+    dummyClient inst;
+    long start;
+    long end;
+    long size;
+    long timer;
+
+
+    Future<Object> obj; //A Future represents the result of an asynchronous computation. Methods are provided to check if the computation is complete, to wait for its completion, and to retrieve the result of the computation.
+    ExecutorService exc;
+
+    public myThread(int fileId, String ip, int port, dummyClient inst, long start, long end) {
+
+        this.fileId = fileId;
+        this.ip = ip;
+        this.port = port;
+        this.inst = inst;
+        this.start = start;
+        this.end = end;
+
+        exc = Executors.newSingleThreadExecutor();
+    }
+
+    public void run() {
+
+        try {
+            obj = exc.submit(() -> {
+                size = inst.getFileSize(ip, port, fileId);
+                return 0;
+            });
+
+            obj.get(timer, TimeUnit.MILLISECONDS); //to throw exception
+        }
+
+        catch (final TimeoutException e) {
+
+            try {
+                size = inst.getFileSize(ip, port, fileId);
+
+            } catch (IOException ex) {
+                System.out.println("IO error");
+            }
+            obj.cancel(true);
+
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+        exc.shutdown();
+    }
+
+    public long getTimer() {
+        return timer;
+    }
+
+
+}
+
+
